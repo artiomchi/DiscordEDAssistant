0f2fab8280bb70b83ff24e9b7d9583fe9f013c7c
Artiom Chilaru <artiom@flexlabs.org>
2016-10-03 11:59:38 +0100

    ! Fixing missed Truncate SP update

--- src/libs/FlexLabs.DiscordEDAssistant.Repositories.EFCore/Base/Migrations/20161003102315_StationSellingModulesJson.cs
+++ src/libs/FlexLabs.DiscordEDAssistant.Repositories.EFCore/Base/Migrations/20161003102315_StationSellingModulesJson.cs
@@ -128,6 +128,22 @@ BEGIN
 
 END
 ");
+
+            migrationBuilder.Sql(@"
+ALTER PROCEDURE [upload].[Eddb_Truncate]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    TRUNCATE TABLE [upload].[Eddb_Commodities]
+    TRUNCATE TABLE [upload].[Eddb_Modules]
+    TRUNCATE TABLE [upload].[Eddb_StarSystems]
+    TRUNCATE TABLE [upload].[Eddb_Stations]
+
+END
+");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -254,6 +270,24 @@ BEGIN
 		VALUES ( [StationID], [Ship] )
 	WHEN NOT MATCHED BY SOURCE THEN
 		DELETE;
+
+END
+");
+
+            migrationBuilder.Sql(@"
+ALTER PROCEDURE [upload].[Eddb_Truncate]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    TRUNCATE TABLE [upload].[Eddb_Commodities]
+    TRUNCATE TABLE [upload].[Eddb_Modules]
+    TRUNCATE TABLE [upload].[Eddb_StarSystems]
+    TRUNCATE TABLE [upload].[Eddb_Stations]
+    TRUNCATE TABLE [upload].[Eddb_Stations_SellingModules]
+    TRUNCATE TABLE [upload].[Eddb_Stations_SellingShips]
 
 END
 ");
