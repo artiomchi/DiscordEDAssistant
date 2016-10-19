dc1c2a9f27d9cd39d26c3d9c99232846d5a68958
Artiom Chilaru <artiom@flexlabs.org>
2016-10-19 22:45:39 +0100

    * Changing the web project to .NET 4.6 * Updating the default namespace

--- src/apps/FlexLabs.EDAssistant.Web/Startup.cs
+++ src/apps/FlexLabs.EDAssistant.Web/Startup.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Net.Http;
 
 namespace FlexLabs.EDAssistant.Web
 {
@@ -35,28 +32,22 @@ namespace FlexLabs.EDAssistant.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+                loggerFactory.AddDebug();
+
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                using (var handler = new HttpClientHandler { Credentials = new NetworkCredential(Configuration["Heartbeat:Login"], Configuration["Heartbeat:Password"])})
-                using (var client = new HttpClient(handler))
-                {
-                    var result = await client.GetAsync(Configuration["Heartbeat:Url"]);
-                }
-
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
