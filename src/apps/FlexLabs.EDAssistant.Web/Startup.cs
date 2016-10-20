5d84cea908c2de4170cecc5b118bd15a6e12fa74
Artiom Chilaru <artiom@flexlabs.org>
2016-10-20 11:26:53 +0100

    + Empty response for now

--- src/apps/FlexLabs.EDAssistant.Web/Startup.cs
+++ src/apps/FlexLabs.EDAssistant.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -48,6 +49,8 @@ namespace FlexLabs.EDAssistant.Web
             }
 
             app.UseMvc();
+
+            app.Use((context, next) => context.Response.WriteAsync(""));
         }
     }
 }
